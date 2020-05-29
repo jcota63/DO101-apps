4840bc75b1165e94406b8ab01e39e93afdf0593a
jcota63 <66097234+jcota63@users.noreply.github.com>
2020-05-29 00:15:32 -0600

    Update app.js

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/mars', function (req, res) {
-  res.send('Hello Marte!\n');
+  res.send('Hello Yoceline!\n');
 });
 
 app.listen(8080, function () {
